0e61a4d1da06e220a62e06eb12325671388c5669
gabe.morias@gmail.com <your.email@example.com>
2025-06-30 18:25:47 -0400

    change styling and position

--- gui/src/pages/gui/index.tsx
+++ gui/src/pages/gui/index.tsx
@@ -7,15 +7,19 @@ export default function GUI() {
   const [isHistoryHidden, setIsHistoryHidden] = useState(false);
   return (
     <div className="flex w-screen flex-row overflow-hidden">
-      <Button
-        className="4xl:flex hidden"
-        onClick={() => setIsHistoryHidden(!isHistoryHidden)}
-      ></Button>
       {!isHistoryHidden && (
-        <aside className="4xl:flex border-vsc-input-border no-scrollbar hidden w-96 overflow-y-auto border-0 border-r border-solid">
+        <aside className="4xl:flex no-scrollbar hidden w-96 overflow-y-auto border-0 border-r border-solid">
           <History />
         </aside>
       )}
+      <div className="4xl:flex hidden items-center">
+        <Button
+          className="relative flex h-32 w-2 flex-col items-center justify-center rounded-full border shadow-sm transition-all duration-200 hover:from-gray-300 hover:to-gray-400"
+          onClick={() => setIsHistoryHidden(!isHistoryHidden)}
+        >
+          <div className="h-10 w-0.5 rounded-full bg-white opacity-70 shadow-sm"></div>
+        </Button>
+      </div>
       <main className="flex flex-1 flex-col overflow-y-auto">
         <Chat />
       </main>
